612c0624d19bd3795d6d6833b9ffe7c2a04b7718
MagonxESP <janma.360@gmail.com>
2020-09-15 19:18:56 +0200

    disconnect from voice channel on stop audio

--- pekora/bot.py
+++ pekora/bot.py
@@ -4,23 +4,43 @@ import os
 import pekora
 
 
-bot = commands.Bot(command_prefix="peko!")
+COMMAND_PREFIX = "peko!"
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
-def play_its_me_pekora(client: discord.VoiceClient):
-    client.play(
-        discord.FFmpegPCMAudio(os.path.join(pekora.AUDIO_DIR, 'Its_me_pekora.mp3')),
-        after=lambda e: client.disconnect()
-    )
+async def play_audio(author: discord.Member, audio_file_name: str):
+    if author.voice:
+        channel: discord.VoiceChannel = author.voice.channel
+        client: discord.VoiceClient = await channel.connect()
+
+        client.play(discord.FFmpegPCMAudio(os.path.join(pekora.AUDIO_DIR, audio_file_name)))
+
+        while client.is_playing():
+            pass
+
+        await client.disconnect()
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    message_text: str = message.content
+    channel: discord.abc.Messageable = message.channel
+
+    if message.author == bot.user:
+        return
+
+    if message_text.startswith(COMMAND_PREFIX):
+        await bot.process_commands(message)
+        return
+
+    if "peko" in message_text:
+        await channel.send("It’s a me, Pekora!")
+        await play_audio(message.author, "Its_me_pekora.mp3")
 
 
 @bot.command()
 async def hi(context: commands.Context):
+    """It’s a me, Pekora!"""
     author: discord.Member = context.message.author
-    channel: discord.VoiceChannel = author.voice.channel
-
-    if channel:
-        voice_channel: discord.VoiceClient = await channel.connect()
-        play_its_me_pekora(voice_channel)
-
-
+    await context.send("It’s a me, Pekora!")
+    await play_audio(author, "Its_me_pekora.mp3")
