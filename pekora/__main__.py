74ed0ad70c287000902a523e273dfabe84692c21
MagonxESP <janma.360@gmail.com>
2020-09-26 19:27:18 +0200

    main funcion and database connection

--- pekora/__main__.py
+++ pekora/__main__.py
@@ -1,10 +1,28 @@
 import asyncio
+import pekora.entitys
 import pekora
 from pekora.bot import bot
 from pekora.http import http
+import os
 
 
-main_loop = asyncio.get_event_loop()
-bot.loop = main_loop
-main_loop.create_task(http.run_task('0.0.0.0', 5000))
-bot.run(pekora.TOKEN)
+def main():
+    if os.path.exists(pekora.DB_DIR) is False:
+        os.mkdir(pekora.DB_DIR)
+
+    pekora.entitys.db.bind(
+        provider='sqlite',
+        filename=os.path.join(pekora.DB_DIR, 'database.sqlite'),
+        create_db=True
+    )
+
+    pekora.channel_feed.subscribe(pekora.HTTP_BASE_URL + '/webhook/pekora/feed')
+
+    main_loop = asyncio.get_event_loop()
+    bot.loop = main_loop
+    main_loop.create_task(http.run_task('0.0.0.0', 3000))
+    bot.run(pekora.TOKEN)
+
+
+if __name__ == '__main__':
+    main()
