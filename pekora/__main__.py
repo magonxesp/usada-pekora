29e94742fb03c9b182170ba904bd9dca460b34c5
MagonxESP <janma.360@gmail.com>
2020-10-18 01:18:34 +0200

    schedule subscribe to youtube feed each 25 hours

--- pekora/__main__.py
+++ pekora/__main__.py
@@ -3,6 +3,7 @@ import pekora.entitys
 import pekora
 from pekora.bot import bot
 from pekora.http import http
+from pekora.jobs import start_jobs
 import os
 
 
@@ -21,6 +22,7 @@ def main():
     main_loop = asyncio.get_event_loop()
     bot.loop = main_loop
     main_loop.create_task(http.run_task('0.0.0.0', 3000))
+    start_jobs(main_loop)
     bot.run(pekora.TOKEN)
 
 
