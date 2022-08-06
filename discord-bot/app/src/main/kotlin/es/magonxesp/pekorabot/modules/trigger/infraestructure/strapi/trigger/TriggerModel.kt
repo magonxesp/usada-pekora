088210373d4e0bd01e68370903248ea4e19c2959
MagonxESP <janma.360@gmail.com>
2022-08-06 21:51:22 +0200

    remove unused imports

--- discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/modules/trigger/infraestructure/strapi/trigger/TriggerModel.kt
+++ discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/modules/trigger/infraestructure/strapi/trigger/TriggerModel.kt
@@ -1,7 +1,5 @@
 package es.magonxesp.pekorabot.modules.trigger.infraestructure.strapi.trigger
 
-import es.magonxesp.pekorabot.modules.shared.infraestructure.strapi.resources.File
-import es.magonxesp.pekorabot.modules.shared.infraestructure.strapi.resources.FileCollection
 import es.magonxesp.pekorabot.modules.shared.infraestructure.strapi.resources.Model
 import kotlinx.serialization.Serializable
 import es.magonxesp.pekorabot.modules.trigger.domain.Trigger
