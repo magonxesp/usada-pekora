fa6ccd9e7dd8850063193f34bb0e989cfacf3e8c
MagonxESP <janma.360@gmail.com>
2022-08-31 21:04:56 +0200

    fix cache keys

--- discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/modules/video/infraestructure/discord/DiscordTextChannelVideoNotifier.kt
+++ discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/modules/video/infraestructure/discord/DiscordTextChannelVideoNotifier.kt
@@ -11,7 +11,8 @@ import kotlinx.coroutines.runBlocking
 
 class DiscordTextChannelVideoNotifier(private val cache: KeyValueCacheStorage) : VideoFeedNotifier {
     override fun notify(video: Video, target: String) {
-        val lastVideoCacheKey = "feed_last_video_id_sent"
+        val lastVideoCacheKey = "feed_last_video_id_sent_$target"
+        val lastVideoMessageCacheKey = "feed_last_video_${video.id}_message_$target"
         val lastVideoId = cache.get(lastVideoCacheKey)
         val channel = discordClient.getChannelById(Snowflake.of(target))
         val content = "**${video.title}** ${video.url}"
@@ -20,9 +21,9 @@ class DiscordTextChannelVideoNotifier(private val cache: KeyValueCacheStorage) :
             if (lastVideoId != video.id) {
                 val message = channel.createMessage(content).awaitSingle()
                 cache.set(lastVideoCacheKey, video.id)
-                cache.set("feed_last_video_${video.id}_message", message.id().toString())
+                cache.set(lastVideoMessageCacheKey, message.id().toString())
             } else {
-                val messageId = cache.get("feed_last_video_${video.id}_message")
+                val messageId = cache.get(lastVideoMessageCacheKey)
                 val editRequest = ImmutableMessageEditRequest.builder().contentOrNull(content).build()
                 channel.getRestMessage(Snowflake.of(messageId!!)).edit(editRequest).awaitSingle()
             }
