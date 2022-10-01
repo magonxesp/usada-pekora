df2c0db583b1a1eab4ea9bba778c5d324a9ffac6
MagonxESP <janma.360@gmail.com>
2022-10-01 11:01:57 +0200

    log discord message requests

--- discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/discord/event/MessageCreateEvent.kt
+++ discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/discord/event/MessageCreateEvent.kt
@@ -1,6 +1,7 @@
 package es.magonxesp.pekorabot.discord.event
 
 import discord4j.core.event.domain.message.MessageCreateEvent
+import kotlinx.coroutines.reactor.awaitSingle
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -9,10 +10,15 @@ private val logger = Logger.getLogger("MessageCreateEvent")
 suspend fun MessageCreateEvent.handleEvents() {
     try {
         val author = message.author.get()
+        val guild = message.guild.awaitSingle()
+
+        logger.info("Message received from discord by ${author.username} on guild ${guild.name} (${guild.id.asString()}); message id ${message.id.asString()}")
 
         if (author.isBot) {
             return
         }
+
+        logger.info("Handling message events for message id ${message.id.asString()}")
 
         if (handleCommand()) return
         if (handleTrigger()) return
