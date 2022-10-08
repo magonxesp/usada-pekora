9f34bf85215ed531bd67d42faaaa320db8e963da
MagonxESP <janma.360@gmail.com>
2022-10-09 01:00:08 +0200

    remove unused classes

--- discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/modules/shared/infraestructure/prometheus/Metric.kt
+++
@@ -1,9 +0,0 @@
-package es.magonxesp.pekorabot.modules.shared.infraestructure.prometheus
-
-enum class Metric(val value: String) {
-    DISCORD_MESSAGE("pekorabot_discord_message_total"),
-    DISCORD_MESSAGE_PROCESSED("pekorabot_discord_message_processed_total"),
-    DISCORD_TRIGGER_FIRED("pekorabot_discord_trigger_fired_total"),
-    DISCORD_COMMAND_FIRED("pekorabot_discord_command_fired_total"),
-    DISCORD_GUILDS("pekorabot_discord_guilds_total"),
-}
