ba0c1c835767ae11b286f90876752ecca0e7367a
MagonxESP <janma.360@gmail.com>
2022-10-15 23:13:25 +0200

    thrown exception on unauthenticated response

--- frontend/modules/shared/infraestructure/discord/client.ts
+++ frontend/modules/shared/infraestructure/discord/client.ts
@@ -1,17 +1,16 @@
 import { fetch } from 'undici'
+import { DiscordRestClientResponseError, DiscordRestClientUnauthorizedError } from './client-error'
 
 
-class RestClientResponseError extends Error { }
-
-export class RestClient {
+export class DiscordRestClient {
 
   private static apiVersion = 10
-  private static baseUrl = `https://discord.com/api/v${RestClient.apiVersion}`
+  private static baseUrl = `https://discord.com/api/v${DiscordRestClient.apiVersion}`
 
   constructor(private bearerToken: string) { }
 
   async request(method: string, endpoint: string, body?: string): Promise<any> {
-    const response = await fetch(`${RestClient.baseUrl}${endpoint}`, {
+    const response = await fetch(`${DiscordRestClient.baseUrl}${endpoint}`, {
       method,
       body,
       headers: {
@@ -19,8 +18,12 @@ export class RestClient {
       }
     })
 
+    if (response.status === 401) {
+      throw new DiscordRestClientUnauthorizedError()
+    }
+
     if (!response.ok) {
-      throw new RestClientResponseError(`An error occurred on request response with status code ${response.status} with response body: ${await response.text()}`)
+      throw new DiscordRestClientResponseError(`An error occurred on request response with status code ${response.status} with response body: ${await response.text()}`)
     }
 
     return response.json()
