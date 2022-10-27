d673f1756c79b57638e8b946ec09aef72cb5b312
MagonxESP <janma.360@gmail.com>
2022-10-27 19:51:21 +0200

    unlimit threads can spawn spring boot and mono coroutine

--- discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/discord/Bot.kt
+++ discord-bot/app/src/main/kotlin/es/magonxesp/pekorabot/discord/Bot.kt
@@ -3,19 +3,14 @@ package es.magonxesp.pekorabot.discord
 import discord4j.core.DiscordClient
 import es.magonxesp.pekorabot.discordBotToken
 import es.magonxesp.pekorabot.modules.shared.domain.thread.ExitOnThreadUncaughtException
-import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.reactor.mono
-import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 val discordClient: DiscordClient = DiscordClient.create(discordBotToken)
 
 fun connectBot() {
     discordClient.withGateway {
-        val dispatcher = Executors.newFixedThreadPool(4)
-            .asCoroutineDispatcher()
-
-        mono(dispatcher) {
+        mono {
             it.handleEvents()
         }
     }.block()
