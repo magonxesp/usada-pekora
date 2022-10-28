4ba8766fcff7acb547c9d79d75521dc34a281912
MagonxESP <janma.360@gmail.com>
2022-10-29 01:36:36 +0200

    create class .button

--- frontend/components/shared/form/Button.tsx
+++ frontend/components/shared/form/Button.tsx
@@ -11,16 +11,6 @@ export default function Button(props: ButtonProps) {
   const color = (props.color) ? `bg-${props.color}` : ''
 
   return (
-    <button className={[
-      'py-2',
-      'px-3',
-      'm-1',
-      'text-center',
-      'rounded',
-      'flex',
-      'items-center',
-      color
-    ].join(' ')}
-    onClick={() => props.onClick && props.onClick(props.value)}>{props.children}</button>
+    <button className={color} onClick={() => props.onClick && props.onClick(props.value)}>{props.children}</button>
   )
 }
