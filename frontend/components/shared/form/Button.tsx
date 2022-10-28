6eaada2f365107d494ec37fa2a6402a12acaa0bc
MagonxESP <janma.360@gmail.com>
2022-10-29 01:56:21 +0200

    use generic type on button value

--- frontend/components/shared/form/Button.tsx
+++ frontend/components/shared/form/Button.tsx
@@ -1,16 +1,20 @@
 import { ColorString } from '../../../modules/shared/domain/color/color'
+import { MouseEvent } from 'react'
 
-interface ButtonProps {
+interface ButtonProps<T> {
   color?: ColorString
   children: any
-  value?: any,
-  onClick?: (value: any) => void
+  value?: T,
+  onClick?: (event: MouseEvent, value: T|undefined) => void
 }
 
-export default function Button(props: ButtonProps) {
+export default function Button<T>(props: ButtonProps<T>) {
   const color = (props.color) ? `bg-${props.color}` : ''
 
   return (
-    <button className={color} onClick={() => props.onClick && props.onClick(props.value)}>{props.children}</button>
+    <button
+      className={color}
+      onClick={(event) => props.onClick && props.onClick(event, props.value)}
+    >{props.children}</button>
   )
 }
