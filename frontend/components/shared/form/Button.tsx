7503f03bba8b4cb35a3e6b37b713acfadd18b6f3
MagonxESP <janma.360@gmail.com>
2022-10-29 18:45:14 +0200

    add InputWrapper and add type prop to Button component

--- frontend/components/shared/form/Button.tsx
+++ frontend/components/shared/form/Button.tsx
@@ -4,17 +4,19 @@ import { MouseEvent } from 'react'
 interface ButtonProps<T> {
   color?: ColorString
   children: any
-  value?: T,
+  value?: T
+  type?: 'button' | 'submit' | 'reset'
   onClick?: (event: MouseEvent, value: T|undefined) => void
 }
 
 export default function Button<T>(props: ButtonProps<T>) {
-  const color = (props.color) ? `bg-${props.color}` : ''
+  const color = (props.color) ? `bg-${props.color}` : 'bg-primary'
 
   return (
     <button
       className={color}
       onClick={(event) => props.onClick && props.onClick(event, props.value)}
+      type={props.type ?? 'button'}
     >{props.children}</button>
   )
 }
---
+++ frontend/components/shared/form/input-wrapper/InputWrapper.tsx
@@ -0,0 +1,62 @@
+import React, { ChangeEvent, useState } from 'react'
+import styles from './InputWrapper.module.css'
+
+interface InputChangeEventHandler {
+  (event: ChangeEvent<HTMLInputElement|HTMLSelectElement>): void
+}
+
+interface InputWrapperProps {
+  label: string
+  children: (eventHandler: InputChangeEventHandler) => React.ReactElement
+  className?: string
+
+  /**
+   * onChange event
+   *
+   * @param {string} name
+   * @param {any} value
+   */
+  onChange?: (name: string, value: any) => void
+
+  /**
+   * Validate the input value when onChange event is fired
+   *
+   * @param {string} value
+   *
+   * @throws {string}
+   *    Throws string with the error message in case the value is not valid
+   */
+  validate?: (value: any) => void
+}
+
+export default function InputWrapper({ label, children, onChange, validate, className }: InputWrapperProps) {
+  const [error, setError] = useState('')
+
+  const handleOnChange: InputChangeEventHandler = (event) => {
+    setError('')
+
+    if (typeof onChange !== 'undefined') {
+      onChange(event.target.name, event.target.value)
+    }
+
+    if (typeof validate !== 'undefined') {
+      try {
+        validate(event.target.value)
+      } catch (error) {
+        if (typeof error === 'string') {
+          setError(error)
+        }
+      }
+    }
+  }
+
+  return (
+    <div className={`${styles.wrapper} space-y-2 ${className ?? ''}`}>
+      <label>{label}</label>
+      <>
+        {children(handleOnChange)}
+      </>
+      <span className="text-red-700">{error}</span>
+    </div>
+  )
+}
