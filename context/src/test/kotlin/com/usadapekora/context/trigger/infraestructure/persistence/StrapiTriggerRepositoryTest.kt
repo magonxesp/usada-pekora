a9a9a7cb0695b79426ff2aa2f782bb41e9f850d0
MagonxESP <janma.360@gmail.com>
2022-11-16 19:37:04 +0100

    undo delete strapi trigger repository test

---
+++ context/src/test/kotlin/com/usadapekora/context/trigger/infraestructure/persistence/StrapiTriggerRepositoryTest.kt
@@ -0,0 +1,19 @@
+package com.usadapekora.context.trigger.infraestructure.persistence
+
+import kotlin.test.Test
+
+class StrapiTriggerRepositoryTest {
+
+    @Test
+    fun `should get all triggers`() {
+        val repository = StrapiTriggerRepository()
+        repository.all()
+    }
+
+    @Test
+    fun `should get all triggers by discord server id`() {
+        val repository = StrapiTriggerRepository()
+        repository.findByDiscordServer("754111233619132466")
+    }
+
+}
