a006cfb76f1013a1fa643d57058456a7d132fe96
MagonxESP <janma.360@gmail.com>
2022-11-26 19:09:39 +0100

    create TriggerAudio aggregate

---
+++ context/src/main/kotlin/com/usadapekora/context/trigger/domain/TriggerAudio.kt
@@ -0,0 +1,22 @@
+package com.usadapekora.context.trigger.domain
+
+import com.usadapekora.context.shared.domain.AggregateRoot
+import com.usadapekora.context.shared.domain.file.UploadedFile
+import com.usadapekora.context.shared.domain.valueobject.UuidValueObject
+
+data class TriggerAudio(
+    val id: TriggerAudioId,
+    val trigger: Trigger.TriggerId,
+    val name: TriggerAudioName,
+    val path: TriggerAudioPath
+) : AggregateRoot(), UploadedFile {
+    data class TriggerAudioId(override val value: String) : UuidValueObject(value)
+    data class TriggerAudioName(val value: String)
+    data class TriggerAudioPath(val value: String)
+
+    override fun id(): String = id.value
+    override fun name(): String = name.value
+    override fun path(): String = path.value
+    override fun url(): String = path.value
+    override fun mimeType(): String = "audio/mpeg"
+}
