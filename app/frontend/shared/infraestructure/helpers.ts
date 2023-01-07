035dd7a1f235ed758d8b819739c50a62ee2daec3
MagonxESP <janma.360@gmail.com>
2023-01-07 18:17:01 +0100

    install toast

---
+++ app/frontend/shared/infraestructure/helpers.ts
@@ -0,0 +1,9 @@
+import { toast as reactToast, TypeOptions } from 'react-toastify'
+
+export function toast(content: string, type: TypeOptions) {
+  reactToast(content, {
+    hideProgressBar: true,
+    autoClose: 2000,
+    type
+  })
+}
