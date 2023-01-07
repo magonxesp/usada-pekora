c8c159b3016accbb8de5eb10d54c15aa09a2b2e1
MagonxESP <janma.360@gmail.com>
2023-01-07 18:16:34 +0100

    add key

--- app/frontend/components/views/guild-triggers-view/GuildTriggersView.tsx
+++ app/frontend/components/views/guild-triggers-view/GuildTriggersView.tsx
@@ -51,8 +51,8 @@ export default function GuildTriggersView() {
           <EmptyState />
         ) : (
           <div className="space-y-5">
-            {Array.from(Array(5).keys()).map(() => (
-              <div>
+            {Array.from(Array(5).keys()).map((_, index) => (
+              <div key={index}>
                 <div role="status" className="animate-pulse mb-2">
                   <div className="w-full h-6 bg-gray-200 rounded dark:bg-gray-600"></div>
                 </div>
