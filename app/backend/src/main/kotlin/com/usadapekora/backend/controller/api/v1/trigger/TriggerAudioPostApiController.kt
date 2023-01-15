41cc57d8cc448a648cd61fa1dd4091ed651f4a22
MagonxESP <janma.360@gmail.com>
2023-01-15 17:05:56 +0100

    add consumes parameter

--- app/backend/src/main/kotlin/com/usadapekora/backend/controller/api/v1/trigger/TriggerAudioPostApiController.kt
+++ app/backend/src/main/kotlin/com/usadapekora/backend/controller/api/v1/trigger/TriggerAudioPostApiController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.koin.java.KoinJavaComponent.inject
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.multipart.MultipartFile
@@ -22,7 +23,7 @@ class TriggerAudioPostApiController : ApiController() {
 
     private val triggerAudioCreator: TriggerAudioCreator by inject(TriggerAudioCreator::class.java)
 
-    @PostMapping("")
+    @PostMapping("", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun create(
         @RequestParam("file") file: MultipartFile,
         @RequestParam("id") id: String,
