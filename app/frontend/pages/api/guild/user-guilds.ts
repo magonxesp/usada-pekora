c09333fec04e9a3c36a74b60a934c3c1ebcecbf3
MagonxESP <janma.360@gmail.com>
2023-04-02 21:17:45 +0200

    fix token

--- app/frontend/pages/api/guild/user-guilds.ts
+++ app/frontend/pages/api/guild/user-guilds.ts
@@ -5,7 +5,8 @@ import { DiscordGuildClient } from '../../../shared/api/discord/client'
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await serverSession(req, res)
+  const session: any = await serverSession(req, res)
+  const token: string = session.accessToken ?? ''
 
   if (!session) {
     res.status(200).json([])
@@ -13,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
-    const client = new DiscordGuildClient(session.accessToken as string)
+    const client = new DiscordGuildClient(token)
     const guilds = await client.userGuilds()
     res.status(200).json(guilds)
   } catch (exception) {
