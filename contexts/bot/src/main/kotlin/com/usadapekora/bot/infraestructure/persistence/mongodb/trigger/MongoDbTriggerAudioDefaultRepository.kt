e7f87aa5fbbf7ab644922d5e0e0d88f487f9dc3d
MagonxESP <janma.360@gmail.com>
2023-04-06 17:26:15 +0200

    allow more audio providers and text responses

---
+++ contexts/bot/src/main/kotlin/com/usadapekora/bot/infraestructure/persistence/mongodb/trigger/MongoDbTriggerAudioDefaultRepository.kt
@@ -0,0 +1,42 @@
+package com.usadapekora.bot.infraestructure.persistence.mongodb.trigger
+
+import com.usadapekora.bot.infraestructure.persistence.mongodb.MongoDbRepository
+import com.usadapekora.bot.domain.trigger.Trigger
+import com.usadapekora.bot.domain.trigger.TriggerAudioDefaultRepository
+import com.usadapekora.bot.domain.trigger.TriggerAudioResponseId
+import com.usadapekora.bot.domain.trigger.response.audio.TriggerAudioDefault
+import com.usadapekora.bot.domain.trigger.exception.TriggerAudioResponseException
+import org.litote.kmongo.eq
+import org.litote.kmongo.findOne
+
+class MongoDbTriggerAudioDefaultRepository : MongoDbRepository<TriggerAudioDefault, TriggerAudioDefaultDocument>(
+    collection = "triggerAudioDefault",
+    documentIdProp = TriggerAudioDefaultDocument::id,
+    documentCompanion = TriggerAudioDefaultDocument
+), TriggerAudioDefaultRepository {
+
+    override fun find(id: TriggerAudioResponseId): TriggerAudioDefault {
+        val audio = oneQuery<TriggerAudioDefaultDocument>(collection) { collection ->
+            collection.findOne(TriggerAudioDefaultDocument::id eq id.value)
+        }
+
+        if (audio != null) {
+            return audio.toEntity()
+        }
+
+        throw TriggerAudioResponseException.NotFound("Trigger audio with id $id not found")
+    }
+
+    override fun findByTrigger(id: Trigger.TriggerId): TriggerAudioDefault {
+        val audio = oneQuery<TriggerAudioDefaultDocument>(collection) { collection ->
+            collection.findOne(TriggerAudioDefaultDocument::trigger eq id.value)
+        }
+
+        if (audio != null) {
+            return audio.toEntity()
+        }
+
+        throw TriggerAudioResponseException.NotFound("Trigger audio of trigger with id $id not found")
+    }
+
+}
---
+++ contexts/bot/src/test/kotlin/com/usadapekora/bot/infraestructure/persistence/mongodb/MongoDbTriggerAudioDefaultRepositoryTest.kt
@@ -0,0 +1,70 @@
+package com.usadapekora.bot.infraestructure.persistence.mongodb
+
+import com.usadapekora.bot.domain.trigger.response.audio.TriggerAudioDefaultMother
+import com.usadapekora.bot.domain.trigger.response.audio.TriggerAudioDefault
+import com.usadapekora.bot.domain.trigger.exception.TriggerAudioResponseException
+import com.usadapekora.bot.infraestructure.persistence.mongodb.trigger.MongoDbTriggerAudioDefaultRepository
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MongoDbTriggerAudioDefaultRepositoryTest : MongoDbRepositoryTest<TriggerAudioDefault, MongoDbTriggerAudioDefaultRepository>(
+    repository = MongoDbTriggerAudioDefaultRepository(),
+    mother = TriggerAudioDefaultMother
+) {
+
+    @Test
+    fun `should find trigger audio by id`() {
+        databaseTest {
+            val audio = repository.find(it.id)
+
+            assertEquals(it, audio)
+        }
+    }
+
+    @Test
+    fun `should not find trigger audio by id`() {
+        databaseTest(save = false) {
+            assertThrows<TriggerAudioResponseException.NotFound> {
+                repository.find(it.id)
+            }
+        }
+    }
+
+    @Test
+    fun `should find trigger audio by trigger id`() {
+        databaseTest {
+            val audio = repository.findByTrigger(it.trigger)
+            assertEquals(it, audio)
+        }
+    }
+
+    @Test
+    fun `should not find trigger audio by trigger id`() {
+        databaseTest(save = false) {
+            assertThrows<TriggerAudioResponseException.NotFound> {
+                repository.findByTrigger(it.trigger)
+            }
+        }
+    }
+
+    @Test
+    fun `should save`() {
+        databaseTest(save = false) {
+            repository.save(it)
+            val audio = repository.find(it.id)
+            assertEquals(it, audio)
+        }
+    }
+
+    @Test
+    fun `should delete`() {
+        databaseTest(delete = false) {
+            repository.delete(it)
+            assertThrows<TriggerAudioResponseException.NotFound> {
+                repository.find(it.id)
+            }
+        }
+    }
+
+}
