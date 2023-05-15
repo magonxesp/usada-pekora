701eeb3fc9f5c159dceb54de48b1ec14e51100dd
MagonxESP <janma.360@gmail.com>
2023-05-15 22:47:03 +0200

    compile react library components

--- contexts/shared/packages/ui/tsup.config.js
+++ contexts/shared/packages/ui/tsup.config.js
@@ -3,5 +3,9 @@ import cssModulesPlugin from 'esbuild-css-modules-plugin'
 
 export default defineConfig({
   esbuildPlugins: [cssModulesPlugin()],
-  entry: ['src/index.ts']
+  entry: ['src/index.ts'],
+  dts: true,
+  format: ['esm'],
+  tsconfig: './tsconfig.json',
+  external: ['react']
 })
