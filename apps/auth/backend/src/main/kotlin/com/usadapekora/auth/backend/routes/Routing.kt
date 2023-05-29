12b3617d75881d3a0b4650cd18fe5196f8e405d6
MagonxESP <janma.360@gmail.com>
2023-05-29 19:47:50 +0200

    move routes to Routing.kt

--- apps/auth/backend/src/main/kotlin/com/usadapekora/auth/backend/routes/Routing.kt
+++ apps/auth/backend/src/main/kotlin/com/usadapekora/auth/backend/routes/Routing.kt
@@ -1,12 +1,12 @@
 package com.usadapekora.auth.backend.routes
 
-import com.usadapekora.auth.backend.routes.api.apiRoutesV1
+import com.usadapekora.auth.backend.routes.api.oauth.authorizationProviderRoutesV1
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRoutes() {
     routing {
         oauthRoutes()
-        apiRoutesV1()
+        authorizationProviderRoutesV1()
     }
 }
--- apps/auth/backend/src/main/kotlin/com/usadapekora/auth/backend/routes/api/ApiRoutesV1.kt
+++
@@ -1,8 +0,0 @@
-package com.usadapekora.auth.backend.routes.api
-
-import com.usadapekora.auth.backend.routes.api.oauth.authorizationProviderRoutesV1
-import io.ktor.server.routing.*
-
-fun Route.apiRoutesV1() {
-    authorizationProviderRoutesV1()
-}
