d8af6f6d26bbdab83c6c9bbdfd26cd692d9ac585
MagonxESP <janma.360@gmail.com>
2023-05-29 19:46:07 +0200

    add trigger routes

--- apps/bot/backend/src/main/kotlin/com/usadapekora/bot/backend/routes/Routing.kt
+++ apps/bot/backend/src/main/kotlin/com/usadapekora/bot/backend/routes/Routing.kt
@@ -1,5 +1,6 @@
 package com.usadapekora.bot.backend.routes
 
+import com.usadapekora.bot.backend.routes.api.trigger.triggerV1
 import com.usadapekora.bot.backend.routes.webhook.youtubeWebhook
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
@@ -7,5 +8,6 @@ import io.ktor.server.routing.*
 fun Application.configureRoutes() {
     routing {
         youtubeWebhook()
+        triggerV1()
     }
 }
