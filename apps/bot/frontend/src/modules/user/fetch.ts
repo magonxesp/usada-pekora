88655e9b48c338bf1283cdde2628eb9a89587cc3
MagonxESP <janma.360@gmail.com>
2023-07-15 02:05:33 +0200

    feat: remove unused interfaces

--- apps/bot/frontend/src/modules/user/fetch.ts
+++ apps/bot/frontend/src/modules/user/fetch.ts
@@ -2,14 +2,6 @@ import { backendUrl, headers } from '../shared/client/backend'
 import { User } from '@usada-pekora/shared-user'
 import { NextRequest } from 'next/server'
 
-interface UserResponse {
-  id: string
-  avatar: string|null
-  name: string
-  providerId: string
-  provider: string
-}
-
 export async function fetchCurrentUser(request: NextRequest|null = null): Promise<User|null> {
   try {
     const response = await fetch(backendUrl(`/api/v1/user/me`), {
