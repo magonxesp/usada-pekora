22056cdab122d18642a48e5c7e4825e91249cefa
MagonxESP <janma.360@gmail.com>
2023-08-16 15:31:04 +0200

    fix: add windows end of line on split

--- contexts/auth/src/main/kotlin/com/usadapekora/auth/infrastructure/jwt/auth0/Auth0Extensions.kt
+++ contexts/auth/src/main/kotlin/com/usadapekora/auth/infrastructure/jwt/auth0/Auth0Extensions.kt
@@ -1,6 +1,6 @@
 package com.usadapekora.auth.infrastructure.jwt.auth0
 
 fun String.trimKey(): String
-    = split("\n")
+    = split("\n", "\r")
     .filter { !Regex("^-+[A-Z ]+-+\$").matches(it) }
     .joinToString("")
