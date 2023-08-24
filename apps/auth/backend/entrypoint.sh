4899904893652952e2f7019c265c499775a30271
MagonxESP <janma.360@gmail.com>
2023-08-25 00:14:15 +0200

    feat: create ssl certificates on run

---
+++ apps/auth/backend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# ... do some setup ...
+if [ "$APP_ENV" -eq "test" && ! -d ssl ]; then
+    mkdir ssl
+    openssl genrsa -out ssl/private_rsa.pem 2048
+    openssl pkcs8 -topk8 -inform PEM -outform PEM -in ssl/private_rsa.pem -out ssl/private.pem -nocrypt
+    openssl rsa -in ssl/private.pem -outform PEM -pubout -out ssl/public.pem
+fi
+
+# then run the CMD passed as command-line arguments
+exec "$@"
