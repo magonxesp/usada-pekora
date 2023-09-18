7ee21aaf19727a14fe38253a78e97bdeb7bfbd5b
MagonxESP <janma.360@gmail.com>
2023-09-18 21:16:55 +0200

    fix: fix auth backend docker test entrypoint

--- apps/auth/backend/test-entrypoint.sh
+++ apps/auth/backend/test-entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ "$APP_ENV" == "test" ] && [ ! -d ssl ]; then
+    mkdir ssl
+    openssl genrsa -out ssl/private_rsa.pem 2048
+    openssl pkcs8 -topk8 -inform PEM -outform PEM -in ssl/private_rsa.pem -out ssl/private.pem -nocrypt
+    openssl rsa -in ssl/private.pem -outform PEM -pubout -out ssl/public.pem
+fi
+
 echo "Executing tests through test entrypoint..."
 eval "$@"
 test_status=$?
@@ -14,7 +21,7 @@ then
     echo "Sending test report to $REPORT_EMAIL_RECEIVER"
 
     mkdir -p /tmp/reports/contexts
-    mkdir -p /tmp/reports/apps/bot
+    mkdir -p /tmp/reports/apps/auth
 
     if [ -d "/test/contexts/shared/build/reports" ]; then
         cp -r /test/contexts/shared/build/reports /tmp/reports/contexts/shared
